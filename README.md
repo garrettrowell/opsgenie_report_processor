6f5b934accb2b5b39cebc5b7f7218b36d5a7d5ac
Garrett Rowell <garrett@puppet.com>
2023-06-20 16:21:28 -0400

    refactor and document usage

--- README.md
+++ README.md
@@ -1,117 +1,47 @@
 # opsgenie_report_processor
 
-Welcome to your new module. A short overview of the generated parts can be found
-in the [PDK documentation][1].
-
-The README template below provides a starting point with details about what
-information to include in your README.
-
 ## Table of Contents
 
 1. [Description](#description)
-1. [Setup - The basics of getting started with opsgenie_report_processor](#setup)
-    * [What opsgenie_report_processor affects](#what-opsgenie_report_processor-affects)
-    * [Setup requirements](#setup-requirements)
-    * [Beginning with opsgenie_report_processor](#beginning-with-opsgenie_report_processor)
-1. [Usage - Configuration options and additional functionality](#usage)
-1. [Limitations - OS compatibility, etc.](#limitations)
-1. [Development - Guide for contributing to the module](#development)
+1. [Setup](#setup)
+1. [Testing Functionality](#testing-functionality)
 
 ## Description
 
-Briefly tell users why they might want to use your module. Explain what your
-module does and what kind of problems users can solve with it.
-
-This should be a fairly short description helps the user decide if your module
-is what they want.
+A Puppet report processor that creates an alert in Opsgenie if an agent fails a puppet run.
+On successful agent runs, Opsgenie is checked for an open Alert. If an alert is found, it is gets closed.
 
 ## Setup
 
-### What opsgenie_report_processor affects **OPTIONAL**
+1. Add this module to your Puppetfile and deploy the code.
+1. On your primary puppet server create `/etc/puppetlabs/puppet/opsgenie.yaml` with the following content (using your own api key):
+    ```yaml
+    api_key: '9r2ku2yc-it85-q4oa-hf22-ahahiq4mphes'
+    ```
+1. On your primary puppet server modify `/etc/puppetlabs/puppet/puppet.conf` so that the `reports` setting includes `opsgenie`. For example:
+    ```yaml
+    [master]
+    reports = puppetdb,opsgenie
+    ```
 
-If it's obvious what your module touches, you can skip this section. For
-example, folks can probably figure out that your mysql_instance module affects
-their MySQL instances.
+## Testing Functionality
 
-If there's more that they should know about, though, this is the place to
-mention:
+One way to test this report processor is by adding the following Puppet code to some node(s)
 
-* Files, packages, services, or operations that the module will alter, impact,
-  or execute.
-* Dependencies that your module automatically installs.
-* Warnings or other important notices.
-
-### Setup Requirements **OPTIONAL**
-
-If your module requires anything extra before setting up (pluginsync enabled,
-another module, etc.), mention it here.
-
-If your most recent release breaks compatibility or requires particular steps
-for upgrading, you might want to include an additional "Upgrading" section here.
-
-### Beginning with opsgenie_report_processor
-
-The very basic steps needed for a user to get the module up and running. This
-can include setup steps, if necessary, or it can be an example of the most basic
-use of the module.
-
-## Usage
-
-Include usage examples for common use cases in the **Usage** section. Show your
-users how to use your module to solve problems, and be sure to include code
-examples. Include three to five examples of the most important or common tasks a
-user can accomplish with your module. Show users how to accomplish more complex
-tasks that involve different types, classes, and functions working in tandem.
-
-## Reference
-
-This section is deprecated. Instead, add reference information to your code as
-Puppet Strings comments, and then use Strings to generate a REFERENCE.md in your
-module. For details on how to add code comments and generate documentation with
-Strings, see the [Puppet Strings documentation][2] and [style guide][3].
-
-If you aren't ready to use Strings yet, manually create a REFERENCE.md in the
-root of your module directory and list out each of your module's classes,
-defined types, facts, functions, Puppet tasks, task plans, and resource types
-and providers, along with the parameters for each.
-
-For each element (class, defined type, function, and so on), list:
-
-* The data type, if applicable.
-* A description of what the element does.
-* Valid values, if the data type doesn't make it obvious.
-* Default value, if any.
-
-For example:
-
-```
-### `pet::cat`
-
-#### Parameters
-
-##### `meow`
-
-Enables vocalization in your cat. Valid options: 'string'.
-
-Default: 'medium-loud'.
+```puppet
+if $facts['fail_catalog'] == 'true' {
+  fail('i was told to fail...')
+}
 ```
 
-## Limitations
+Then when you want a catalog to fail simply create `/etc/puppetlabs/facter/facts.d/fail_catalog.txt` with the following content:
 
-In the Limitations section, list any incompatibilities, known issues, or other
-warnings.
+```txt
+fail_catalog=true
+```
 
-## Development
+And when you want a successful run:
 
-In the Development section, tell other users the ground rules for contributing
-to your project and how they should submit their work.
-
-## Release Notes/Contributors/Etc. **Optional**
-
-If you aren't using changelog, put your release notes here (though you should
-consider using changelog). You can also add any additional sections you feel are
-necessary or important to include here. Please use the `##` header.
-
-[1]: https://puppet.com/docs/pdk/latest/pdk_generating_modules.html
-[2]: https://puppet.com/docs/puppet/latest/puppet_strings.html
-[3]: https://puppet.com/docs/puppet/latest/puppet_strings_style.html
+```txt
+fail_catalog=false
+```
--- lib/puppet/reports/opsgenie.rb
+++ lib/puppet/reports/opsgenie.rb
@@ -6,105 +6,104 @@ require 'yaml'
 require 'erb'
 
 Puppet::Reports.register_report(:opsgenie) do
+  desc 'Open an Opsgenie Alert on failed puppet runs'
+
   configfile = File.join([File.dirname(Puppet.settings[:config]), 'opsgenie.yaml'])
-  reaise(Puppet::ParseError, "OpsGenie report config file #{configfile} not readable") unless File.exist?(configfile)
+  raise(Puppet::ParseError, "Opsgenie report config file #{configfile} not readable") unless File.exist?(configfile)
   begin
     config = YAML.load_file(configfile)
   rescue TypeError => e
-    raise Puppet::ParseError, "OpsGenie Yaml file is invalid. #{e.message}"
+    raise Puppet::ParseError, "Opsgenie Yaml file is invalid. #{e.message}"
   end
 
   API_KEY = config['api_key'] || nil
   API_BASE_URI = config['api_uri'] || 'https://api.opsgenie.com/v2/alerts'
 
-  desc <<-DESC
-  Send notification of failed reports to OpsGenie
-  DESC
+  raise(Puppet::Error, "api_key must be set in #{configfile}") if API_KEY.nil?
+
+  # authentication header
+  Header = {
+    'Content-Type'  => 'application/json',
+    'Authorization' => "GenieKey #{API_KEY}"
+  }
+
+  def post(api_uri: API_BASE_URI, header: Header, data: {})
+    uri = URI.parse(api_uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Post.new(uri.request_uri, header)
+    request.body = data.to_json
+    response = http.request(request)
+    return response
+  end
+
+  def get(api_uri: API_BASE_URI, header: Header, data: {})
+    uri = URI.parse(api_uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri.request_uri, header)
+    request.body = data.to_json unless data.empty?
+    response = http.request(request)
+    return response
+  end
+
+  def create_alert(identifier)
+    alert_data = {
+        'message' => "Puppet run for #{self.host} Failed at #{Time.now.asctime}",
+        'alias'   => identifier,
+      }
+
+      # https://docs.opsgenie.com/docs/alert-api#create-alert
+      self.post(data: alert_data)
+  end
+
+  def get_alert(identifier)
+    encoded_identifier = ERB::Util.url_encode(identifier)
+    # https://docs.opsgenie.com/docs/alert-api#get-alert
+    self.get(api_uri: "#{API_BASE_URI}/#{encoded_identifier}?identifierType=alias")
+  end
+
+  def close_alert(identifier)
+    encoded_identifier = ERB::Util.url_encode(identifier)
+    close_data = {
+      'note' => "Puppet run for #{self.host} Succeeded at #{Time.now.asctime}"
+    }
+
+    # https://docs.opsgenie.com/docs/alert-api#close-alert
+    self.post(api_uri: "#{API_BASE_URI}/#{encoded_identifier}/close?identifierType=alias", data: close_data)
+  end
 
   def process
     identifier = "#{self.host} failed_puppetrun"
 
     self.status != nil ? status = self.status : status = 'undefined'
-    Puppet.info(status)
 
     # If node fails open an alert
     if status == 'failed' or status == 'undefined' then
-      uri = URI.parse(API_BASE_URI)
 
-      header = {
-        'Content-Type'  => 'application/json',
-        'Authorization' => "GenieKey #{API_KEY}"
-      }
-
-      data = {
-        'message' => "Puppet run for #{self.host} #{status} at #{Time.now.asctime}",
-        'alias'   => identifier,
-      }
-
-      # Create HTTP Objects
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      request = Net::HTTP::Post.new(uri.request_uri, header)
-      request.body = data.to_json
-
-      # Send the request
-      response = http.request(request)
-      # Todo: response error handling
-      Puppet.info(response.inspect)
+      create_alert_response = self.create_alert(identifier)
+      Puppet.info("create_alert: #{create_alert_response.body}")
 
     # If the node is successful:
     #  - First check if there is an open alert for the given node.
     #  - If there is an open alert, close it.
     elsif status == 'changed' or status == 'unchanged'
-      node_alias = ERB::Util.url_encode(identifier)
-      uri = URI.parse("#{API_BASE_URI}/#{node_alias}?identifierType=alias")
+      get_alert_response = self.get_alert(identifier)
 
-      header = {
-        'Content-Type'  => 'application/json',
-        'Authorization' => "GenieKey #{API_KEY}"
-      }
-
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      request = Net::HTTP::Get.new(uri.request_uri, header)
-
-      # Send the request
-      response = http.request(request)
-
-      case response
+      case get_alert_response
         when Net::HTTPSuccess
-          response_body = JSON.parse(response.body)
+          response_body = JSON.parse(get_alert_response.body)
           # Check if there is an open alert for said node
           if response_body['data']['status'] == 'open'
             Puppet.info("Alert open for #{self.host}")
             # Now that our node is healty close the alert
-            uri = URI.parse("#{API_BASE_URI}/#{node_alias}/close?identifierType=alias")
-
-            header = {
-              'Content-Type'  => 'application/json',
-              'Authorization' => "GenieKey #{API_KEY}"
-            }
-
-            data = {
-              'note' => "Puppet run for #{self.host} #{status} at #{Time.now.asctime}"
-            }
-
-            http = Net::HTTP.new(uri.host, uri.port)
-            http.use_ssl = true
-            request = Net::HTTP::Post.new(uri.request_uri, header)
-            request.body = data.to_json
-
-            # Send the request
-            response = http.request(request)
-            # Todo: response error handling
-            Puppet.info(response.body)
-
+            close_alert_response = self.close_alert(identifier)
+            Puppet.info("close_alert: #{close_alert_response.body}")
           else
             Puppet.info("No alert currently for #{self.host}")
           end
         else
-          # Todo: response error handling
-          Puppet.info(response.body)
+          Puppet.info("get_alert: #{get_alert_response.body}")
       end
     end
   end
